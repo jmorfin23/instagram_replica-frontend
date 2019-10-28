2af027d67afcf9ff8d928c9962478f76bbed6586
Jonathan Morfin <jmorfin776@gmail.com>
2019-10-28 16:10:59 -0400

    feed page navbar completed TODO: posts and profile sections

--- src/components/feedNav/index.js
+++ src/components/feedNav/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './index.css';
 import Instagram_logo2 from '../../images/instagram_logo2.png'
 import Instalogo from '../../images/instagram.logo.png';
@@ -7,26 +7,35 @@ function FeedNav() {
   return (
     <div className="feedNav">
       <div className="navbox1">
+
         <div className="navbox1_1">
           <img src={Instalogo} alt="instagram logo"></img>
         </div>
-
         <div className="navbox1_2"></div>
-
         <div className="navbox1_3">
           <img src={Instagram_logo2} alt="Instagram_logo"></img>
         </div>
+      </div>
 
-      </div>
       <div className="navbox2">
-      <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+      <input className="form-control" type="text" placeholder="Search" aria-label="Search"></input>
       </div>
+
       <div className="navbox3">
+      <div className="navbox3_1">
+        <i class="far fa-compass fa-lg"></i>
       </div>
+      <div className="navbox3_2">
+        <i class="far fa-heart fa-lg"></i>
+      </div>
+      <div className="navbox3_3">
+        <i class="far fa-user fa-lg"></i>
+      </div>
+      </div>
+
     </div>
 
   );
 }
-
 
 export default FeedNav;
--- src/views/feed_Page/index.js
+++ src/views/feed_Page/index.js
@@ -8,7 +8,23 @@ class Feed_Page extends Component {
   render() {
     return (
       <div className="feed_page">
+        <nav>
           <FeedNav />
+        </nav>
+        <section>
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <div className="row">
+              <div className="col-md-8">
+                <h1>haskdfj;laskdjf;laksdjfaasdfasdf</h1>
+              </div>
+              <div className="col-md-4">
+                <h2>a;ksdjfljasldfja</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        </section>
         </div>
 
     );
