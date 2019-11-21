71341eeae44832e510678b367165a0ef5ddb2f01
Jonathan Morfin <jmorfin776@gmail.com>
2019-11-21 14:00:48 -0500

    profile header section complete

--- src/views/profile_Page/index.js
+++ src/views/profile_Page/index.js
@@ -20,18 +20,56 @@ const Profile_Page = () => {
               <div className="pw_1">
                 <img src={user.url} alt="profile pic"></img>
               </div>
-
             <div className="pw_2">
-              <div>
-                <div><h1>{user.username}</h1></div>
-                <ul></ul>
+              <div className="pw_2_1">
+                <div><h4>{user.username}</h4></div>
+                <a href="#">
+                  <button className="" type="button">Edit Profile</button>
+                </a>
+                <div>
+                  <button><i className="fas fa-cog fa-lg"></i></button>
+                </div>
               </div>
-
+              <ul>
+                <li><span><span>0</span>  posts</span></li>
+                <li><span><span>0</span>  followers</span></li>
+                <li><span><span>0</span>  following</span></li>
+              </ul>
               <div><h6>{user.fullname}</h6></div>
             </div>
           </div>
+          <div className="bp">
+            <a className="bp1 bp1_2 " href="#">
+              <span>
+                <i class="fa fa-border-all icon"></i>
+                <span>POSTS</span>
+              </span>
+            </a>
+            <a className="bp1 bp1_2" href="#">
+              <span>
+                <i class="fas fa-tv icon"></i>
+                <span>IGTV</span>
+              </span>
+            </a>
+            <a className="bp1 bp1_2" href="#">
+              <span>
+                <i class="far fa-bookmark icon"></i>
+                <span>SAVED</span>
+              </span>
+            </a>
+            <a className="bp1"href="#">
+              <span>
+                <i class="far fa-id-badge icon"></i>
+                <span>TAGGED</span>
+              </span>
+            </a>
+          </div>
           </div>
         </div>
+
+        {/* Footer */}
+        <Footer />
+
       </div>
     );
 }
